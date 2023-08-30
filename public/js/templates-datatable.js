2cf62806336a91ca6fb01266e9c48c3b3819e2e8
Esteban Betancur <84148045+EstebanBetancur1@users.noreply.github.com>
2023-08-30 11:38:10 -0500

    fixed templates

--- public/js/templates-datatable.js
+++ public/js/templates-datatable.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         columnDefs: [{targets: 0, checkboxes: {selectRow: true}},{targets: 5, data: null, defaultContent: '<button class="btn btn-primary">Editar</button>'}],
         select: {style:'multi'},
         order: [['1', 'asc']],
-        columns: [{data: 'id'}, {data: 'nit'}, {data: 'nombre'}, {data: 'documento'}, {data: 'concepto'}],
+        columns: [{data: 'id'}, {data: 'nit'}, {data: 'name'}, {data: 'doc'}, {data: 'concept'}],
         ordering: false,
         language: {url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"},
     });
